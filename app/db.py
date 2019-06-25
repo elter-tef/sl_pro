46bfa8931484ea456af542e8c9c9ed1631778fd2
elter-tef <gorbos@bk.ru>
2019-06-25 20:34:19 +0300

    8

--- app/db.py
+++ app/db.py
@@ -4,6 +4,7 @@ class User(db.Model):
 	__tablename__ = 'user'
 	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 	User_Character = db.relationship('UserCharacter', back_populates='user', lazy='dynamic')
+
 class Character(db.Model):
 	__tablename__ = 'character'
 	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
